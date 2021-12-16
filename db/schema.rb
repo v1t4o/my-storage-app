d255b96f3fb7e44d02a2709e24278864d9a8fe01
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-12-16 02:16:11 -0300

    Testa visita à tela inicial e exibição de galpões cadastrados

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_16_050154) do
+
+  create_table "warehouses", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
