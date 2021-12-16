045d15c882803ca69c6e8f49ff4929922d1ae4b5
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-12-16 14:04:54 -0300

    Testa cadastro de galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,4 +1,17 @@
 class WarehousesController < ApplicationController
+  
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :address,
+                                                         :state, :city, :postal_code, :description,
+                                                         :useful_area, :total_area)
+    warehouse = Warehouse.create(warehouse_params)
+    redirect_to warehouse_path(warehouse.id), notice: 'Galpão registrado com sucesso'
+  end
+
   def show
     @warehouse = Warehouse.find(params[:id])
   end
---
+++ spec/system/register_warehouse_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'Visitante cadastra um galpão' do
+  it 'através de um link na tela inicial' do
+    visit root_path
+    click_on 'Cadastrar novo galpão'
+
+    expect(page).to have_css('h2', text: 'Novo Galpão')
+    expect(page).to have_field 'Nome'
+    expect(page).to have_field 'Código'
+    expect(page).to have_field 'Endereço'
+    expect(page).to have_field 'Cidade'
+    expect(page).to have_field 'Estado'
+    expect(page).to have_field 'CEP'
+    expect(page).to have_field 'Descrição'
+    expect(page).to have_field 'Área Total'
+    expect(page).to have_field 'Área Útil'
+    expect(page).to have_button 'Gravar'
+  end
+
+  it 'com sucesso' do
+    visit root_path
+    click_on 'Cadastrar novo galpão'
+
+    fill_in 'Nome', with: 'Juiz de Fora'
+    fill_in 'Código', with: 'JDF'
+    fill_in 'Endereço', with: 'Av Rio Branco'
+    fill_in 'Cidade', with: 'Juiz de Fora'
+    fill_in 'Estado', with: 'MG'
+    fill_in 'CEP', with: '36000-000'
+    fill_in 'Descrição', with: 'Um galpão mineiro com o pé no Rio'
+    fill_in 'Área Total', with: '5000'
+    fill_in 'Área Útil', with: '3000'
+    click_on 'Gravar'
+
+    expect(page).to have_css('h1', text: 'Juiz de Fora')
+    expect(page).to have_css('h2', text: 'JDF')
+    expect(page).to have_css('dt', text: 'Descrição:')
+    expect(page).to have_css('dd', text: 'Um galpão mineiro com o pé no Rio')
+    expect(page).to have_css('dt', text: 'Endereço:')
+    expect(page).to have_css('dd', text: 'Av Rio Branco - Juiz de Fora/MG')
+    expect(page).to have_css('dt', text: 'CEP:')
+    expect(page).to have_css('dd', text: '36000-000')
+    expect(page).to have_css('dt', text: 'Área Total:')
+    expect(page).to have_css('dd', text: '5000 m2')
+    expect(page).to have_css('dt', text: 'Área Útil:')
+    expect(page).to have_css('dd', text: '3000 m2')
+    expect(page).to have_content 'Galpão registrado com sucesso'
+  end
+end
