d424e3a9aba640eed3d53a48929bb8ac3b2281d2
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-12-17 00:31:31 -0300

    Testa listagem de fornecedores cadastrados

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,4 +1,8 @@
 class SuppliersController < ApplicationController
+  def index
+    @suppliers = Supplier.all
+  end
+  
   def show
     @supplier = Supplier.find(params[:id])
   end
---
+++ spec/system/view_suppliers_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Usuário vê fornecedores' do
+  it 'através de um link na tela inicial' do
+    visit root_path
+
+    expect(page).to have_link('Visualizar fornecedores', href: suppliers_path)
+  end
+
+  it 'cadastrados previamente' do
+    Supplier.create(fantasy_name: 'Samsung', legal_name: 'Samsung do BR LTDA',
+                    eni: '85935972000120', address: 'Av Industrial, 1000, São Paulo',
+                    email: 'financeiro@samsung.com.br', phone: '11 1234-5678')
+    Supplier.create(fantasy_name: 'LG', legal_name: 'LG do Brasil LTDA',
+                    eni: '05979910000181', address: 'Av Industrial, 2000, São Paulo',
+                    email: 'vendas@lg.com.br', phone: '21 9876-4321')
+    visit root_path
+    click_on 'Visualizar fornecedores'
+    expect(page).to have_content 'Samsung'
+    expect(page).to have_content '85935972000120'
+    expect(page).to have_content 'LG'
+    expect(page).to have_content '05979910000181'
+  end
+
+  it 'e não existem fornecedores' do
+    visit root_path
+    click_on 'Visualizar fornecedores'
+    expect(page).to have_content 'Nenhum fornecedor cadastrado'
+  end
+end
