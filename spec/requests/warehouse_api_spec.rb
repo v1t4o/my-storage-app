6d5e01ac6cd0852d800dc24d2a304d4703ef81cb
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2022-01-09 21:47:13 -0300

    Adiciona endpoint de consulta de galpões

--- spec/requests/warehouse_api_spec.rb
+++ spec/requests/warehouse_api_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Warehouse API' do
   context 'GET /api/v1/warehouses' do
     it 'com sucesso' do
-      Warehouse.create(name: 'Guarulhos', code: 'GRU', description: 'Ótimo galpão numa linda cidade',
+      Warehouse.create!(name: 'Guarulhos', code: 'GRU', description: 'Ótimo galpão numa linda cidade',
                       address: 'Av Fernandes Lima', city: 'São Paulo', state: 'SP',
                       postal_code: '57050-021', total_area: 10000, useful_area: 8000)
-      Warehouse.create(name: 'Porto Alegre', code: 'POA', description: 'Ótimo galpão numa linda cidade',
+      Warehouse.create!(name: 'Porto Alegre', code: 'POA', description: 'Ótimo galpão numa linda cidade',
                       address: 'Av Josias Souza', city: 'Porto Alegre', state: 'RS',
                       postal_code: '57050-050', total_area: 10000, useful_area: 8000)
 
@@ -22,13 +22,44 @@ describe 'Warehouse API' do
       expect(response.body).not_to include 'Av Josias Souza'
     end
 
-    it 'quando retorna sem dados' do
+    it 'retorna resposta vazia quando não há galpões cadastrados' do
       get '/api/v1/warehouses'
 
       parsed_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(response.content_type).to include('application/json')
       expect(parsed_response).to eq []
+    end
+  end
+
+  context 'GET /api/v1/warehouses/:id' do
+    it 'com sucesso' do
+      warehouse = Warehouse.create!(name: 'Guarulhos', code: 'GRU', description: 'Ótimo galpão numa linda cidade',
+                      address: 'Av Fernandes Lima', city: 'São Paulo', state: 'SP',
+                      postal_code: '57050-021', total_area: 10000, useful_area: 8000)
+      
+      get "/api/v1/warehouses/#{warehouse.id}"
+
+      parsed_response = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to include('application/json')
+      expect(parsed_response["name"]).to eq 'Guarulhos'
+      expect(parsed_response["code"]).to eq 'GRU'
+      expect(parsed_response["description"]).to eq 'Ótimo galpão numa linda cidade'
+      expect(parsed_response["city"]).to eq 'São Paulo'
+      expect(parsed_response["state"]).to eq 'SP'
+      expect(parsed_response["postal_code"]).to eq '57050-021'
+      expect(parsed_response["total_area"]).to eq 10000
+      expect(parsed_response["useful_area"]).to eq 8000
+      expect(parsed_response.keys).not_to include 'created_at'
+      expect(parsed_response.keys).not_to include 'updated_at'
+      expect(parsed_response.keys).not_to include 'address'
+    end
+
+    it 'galpão não existe' do
+      get '/api/v1/warehouses/777'
+
+      expect(response.status).to eq 404
     end
   end
 end
