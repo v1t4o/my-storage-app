7066e2ce98b4d901cb94904de6f854212b77912d
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2022-01-10 16:32:29 -0300

    Atualiza endpoint de modelos de produto

--- app/controllers/api/v1/product_models_controller.rb
+++ app/controllers/api/v1/product_models_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ProductModelsController < Api::V1::ApiController
   def index
     product_models = ProductModel.all
-    render json: product_models.as_json(except: [:created_at, :updated_at]), status: 200
+    render json: product_models.as_json(except: [:created_at, :updated_at, :height, :width, :length, :supplier_id, :category_id], methods: [:dimensions], include: [{supplier: {only: :fantasy_name}}, {category: {only: :name}}]), status: 200
   end
 
   def show
     begin
       product_model = ProductModel.find(params[:id])
-      render json: product_model.as_json(except: [:created_at, :updated_at]), status: 200
+      render json: product_model.as_json(except: [:created_at, :updated_at, :height, :width, :length, :supplier_id, :category_id], methods: [:dimensions], include: [{supplier: {only: :fantasy_name}}, {category: {only: :name}}]), status: 200
     rescue ActiveRecord::RecordNotFound
       render json: '{}', status: 404
     end
